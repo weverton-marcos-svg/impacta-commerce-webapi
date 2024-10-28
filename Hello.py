541d6195c91e1f2fb48705a4005fb68b9138339d
Weverton Marcos <wevertonmarcos.842@gmail.com.br>
2024-10-27 22:53:04 -0300

    Aula 04 - Criando Rota de produtos

--- Hello.py
+++ Hello.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
     return "<p>Hello World!</p>"
+
+
+@app.route("/products")
+def products():
+    response = jsonify([
+        {
+            "title": "Caneca Personalizada de Porcelana Do Backend",
+            "amount": 123.45,
+            "installments": {"number": 3, "total": 41.15, "hasFree": True},
+        },
+        {
+            "title": "Caneca de tulipa",
+            "amount": 789.45,
+            "installments": {"number": 6, "total": 41.15},
+        }
+    ])
+
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response

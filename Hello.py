441f80397ed4e409c2c09d0baa82ac31771f50fe
Weverton Marcos <wevertonmarcos.842@gmail.com.br>
2024-11-15 15:19:04 -0300

    Aula 10 - ontinuous Integration e ajustes no projeto

--- Hello.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def ola_mundo():
-    return "<p>Olá, Mundo!</p>"
---
+++ run.py
@@ -0,0 +1,14 @@
+# import os
+from api import create_app
+from api.model import initialize_db
+import os
+
+flask_env = os.getenv('FLASK_ENV')
+print('FLASK_ENV:' + str(flask_env))
+
+app = create_app(flask_env)
+app.app_context().push()
+
+initialize_db.init()
+
+app.run()
